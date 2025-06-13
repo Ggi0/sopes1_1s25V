fa81ebe9fc15e6f00e68d02bf0f3f9d30f544b51
Ggi0 <gconcohacax21@gmail.com>
2025-06-12 18:18:18 -0600

    rutas completas del recolector

--- Proyecto1_Fase1/api/index.js
+++ Proyecto1_Fase1/api/index.js
@@ -12,12 +12,40 @@ app.get('/', (req, res) => {
 
 app.get('/recolector', async(req, res) => {
     try {
-        const response = await axios.get('http://localhost:8080/metrics');
+        const response = await axios.get('http://localhost:8080');
         res.json(response.data);
     } catch (error) {
         res.status(500).send('Error al tratar de comunicarse con el RECOLECTOR')
     }
-})
+});
+
+app.get('/recolector/metrics', async(req, res) => {
+    try {
+        const response = await axios.get('http://localhost:8080/metrics');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send('Error al tratar de obtener las metricas con el RECOLECTOR')
+    }
+});
+
+app.get('/recolector/info', async(req, res) => {
+    try{
+        const response = await axios.get('http://localhost:8080/info');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send('ERROR al tratar de obtener la info del recolector')
+    }
+});
+
+app.get('/recolector/health', async(req, res) => {
+    try{
+        const response = await axios.get('http://localhost:8080/health');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send('ERROR al tratar de obtener el estado del recolector')
+    }
+});
+
 
 
 app.listen(PORT, () => {
